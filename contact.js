eb9ed202608b23aa5609f70af62b661644ecac1d
leahmarke <lmarke@live.com>
2022-05-29 02:32:48 -0400

    just copied in code from other contact me page for form layout

---
+++ contact.js
@@ -0,0 +1,21 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
+document.querySelectorAll(".nav-link").forEach((n) =>
+  n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  })
+);
+
+/*start js for form*/
+const placeForResults = document.getElementById("results");
+
+new URLSearchParams(window.location.search).forEach((value, name) => {
+  console.log(new URLSearchParams(window.location.search));
+  placeForResults.append(`${name} : ${value}`);
+  placeForResults.append(document.createElement("br"));
+});
