3912d97fbaf0312f714f8a29f9b3c1a4230c466c
leahmarke <lmarke@live.com>
2022-06-03 01:37:38 -0400

    added absolute positioning to all nav bars

--- contact.js
+++ contact.js
@@ -4,3 +4,11 @@ hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
   navMenu.classList.toggle("active");
 });
+
+const placeForResults = document.getElementById("results");
+
+new URLSearchParams(window.location.search).forEach((value, name) => {
+  console.log(new URLSearchParams(window.location.search));
+  placeForResults.append(`${name} : ${value}`);
+  placeForResults.append(document.createElement("br"));
+});
